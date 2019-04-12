ea5010afafaaed5b45b877be61299742e6d8f764
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-04-12 23:02:07 +0200

    feat : validating email

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 let email = document.querySelector(".emailing");
-let emailAddress = "";
-console.log(email);
-if (email.value !== "") {
-    console.log(email.value);
-    emailAddress = email.value;
-} else {
-    email.value = "Please enter an email address";
-}
+let emailAddress = email.value;
+let btnNotify = document.querySelector(".notify");
+let msg = document.querySelector(".message");
 
-if (!emailAddress.includes(".") && !emailAddress.includes("@")) {
-    console.log("enter a valid email address");
-    
+function ValidateEmail() {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailAddress)) {
+        
+        msg.innerHTML = "";
+        return (true)
+    }
+    msg.innerHTML = "You have entered an invalid email address!";
+    email.style.border = "1px solid red";
+    return (false)
 }
+btnNotify.addEventListener("click", ValidateEmail, false);
