61f9ad46a882bef2c71058842591c7b875f82091
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-04-13 10:52:31 +0200

    feat: javascript error code

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 let email = document.querySelector(".emailing");
-let emailAddress = email.value;
+let emailAddress = "";
 let btnNotify = document.querySelector(".notify");
 let msg = document.querySelector(".message");
-
+emailAddress = email.value;
 function ValidateEmail() {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailAddress)) {
-        
-        msg.innerHTML = "";
-        return (true)
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
+    console.log(email.value);
+    if (re.test(email.value)) {       
+        msg.innerHTML = email.value;
+        return (true);
+    } else {
+        msg.innerHTML = "The email provided is not valid!";
+        email.style.focus;
+        email.style.border = "1px solid red";
+        return (false);
     }
-    msg.innerHTML = "You have entered an invalid email address!";
-    email.style.border = "1px solid red";
-    return (false)
 }
+btnNotify.addEventListener("mouseover", function () {
+    msg.innerHTML = "Provide a valid email address!";
+}, false);
+btnNotify.addEventListener("mouseout", function () {
+    msg.innerHTML = "";
+}, false);
+
 btnNotify.addEventListener("click", ValidateEmail, false);
